9c0f29d7b5ffdd25b72ca120538425350357fb02
Arnab Kumar Nandy <arnab.nandy1991@gmail.com>
2020-09-05 12:49:20 +0530

    Added new Shared Library for docker Build and Push

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,19 @@
+def login() {
+    withCredentials([usernamePassword9credentialsId: 'docker.hub', usernameVariable: 'hubUsername', passwordVariable: 'hubPassword')]) {
+        sh """
+            docker login --username="${hubUsername}" --password="${hubPassword}"
+        """
+    }
+}
+
+def build(String tag) {
+    sh """
+        docker build -t "${tag}" .
+    """
+}
+
+def push(String tag) {
+    sh """
+        docker push "${tag}"
+    """
+}
